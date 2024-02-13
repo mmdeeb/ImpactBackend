5eff01e0c8e8caf64f22b709187d7321262eb7d5
Mohammed Yousef <mohammedmohammeddeeb@gmail.com>
2024-02-13 15:03:57 +0300

    aa

--- ImpactWebAPI/Program.cs
+++ ImpactWebAPI/Program.cs
@@ -1,3 +1,9 @@
+using ImpactApplication;
+using ImpactInfrastructure;
+using ImpactPresentation;
+using Serilog;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services
+    .AddApplication()
+    .AddInfrastructure()
+    .AddPresentation();
+
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
 
 var app = builder.Build();
 
