7204e9c1eb38f0579364802a6aaa416bcb86ffec
shadiassi <shadiassi85@gmail.com>
2024-02-28 15:42:19 +0300

    Add authintication to swagger

--- ImpactWebApi/Program.cs
+++ ImpactWebApi/Program.cs
@@ -3,8 +3,10 @@ using ImpactInfrastructure;
 using ImpactInfrastructure.Persistence;
 using ImpactPresentation;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Serilog;
-
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,7 +15,28 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("ImpactWebApi", new Microsoft.OpenApi.Models.OpenApiSecurityScheme()
+    {
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        Description = "Please enter the authintication token"
+    });
+    options.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "ImpactWebApiAuth"
+                }
+            },new List<string>()
+        }
+    });
+});
 
 builder.Services
     .AddApplication()
@@ -23,6 +46,22 @@ builder.Services.AddDbContext<ImpactDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("ImpactWebApi")));
 builder.Host.UseSerilog((context, configuration) =>
     configuration.ReadFrom.Configuration(context.Configuration));
+//add authintication
+builder.Services.AddAuthentication("Bearer").AddJwtBearer(
+options =>
+{
+    options.TokenValidationParameters = new()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Authentiaction:Issuer"],
+        ValidAudience = builder.Configuration["Authentiaction:Audience"],
+        IssuerSigningKey = new
+            SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration["Authentiaction:Secret"])),
+    };
+}
+);
 
 var app = builder.Build();
 
@@ -34,9 +73,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
 
 app.UseAuthorization();
-app.UseAuthentication();
 app.MapControllers();
 
 app.Run();
